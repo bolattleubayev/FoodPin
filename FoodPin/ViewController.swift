de6381fa8c74d841c0f784af11f274d077b5ffe9
Bolat Tleubayev <bolat.tleubayev@nu.edu.kz>
2019-12-12 01:20:11 +0600

    Initial Commit

---
+++ FoodPin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodPin
+//
+//  Created by macbook on 12/12/19.
+//  Copyright © 2019 bolattleubayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
